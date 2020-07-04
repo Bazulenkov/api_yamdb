d94f14e4e6ac1ec01d97217a587623e502fd57f1
Bazulenkov <anton@bazul.ru>
2020-07-04 22:28:57 +0300

    Начал делать рейтинги через сигналы

--- api/apps.py
+++ api/apps.py
@@ -1,5 +1,14 @@
 from django.apps import AppConfig
 
+# from django.db.models.signals import post_save, post_delete
+
 
 class ApiConfig(AppConfig):
     name = "api"
+
+    # def ready(self):
+    #     # importing model classes
+    #     Title = self.get_model("Review")
+    #     # registering signals with the model's string label
+    #     post_save.connect(receiver, sender=api.Review)
+    #     post_delete.connect(receiver, sender=api.Review)
